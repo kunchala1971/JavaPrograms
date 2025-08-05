7f1a8b5da0a2b133a9868d92c415578538689ac8
srinivasarao <kunchala1971@gmail.com>
2025-08-05 11:18:09 +0530

    update

--- ds/1.Searches/LinearSearch.java
+++ ds/1.Searches/LinearSearch.java
@@ -1,20 +1,23 @@
 public class LinearSearch {
     public static int Linear_Search(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target)
-               target= i;
+            if (nums[i] == target) {
+                target = i;
+                return target;
+            }
 
         }
-        return target;
+        return -1;
     }
+
     public static void main(String args[]) {
         int nums[] = { 5, 7, 9, 11, 13 };
-        int target = 11;
+        int target = 5;
         int result = Linear_Search(nums, target);
         if (result != -1) {
             System.out.println("Element Found at " + result);
         } else {
-            System.out.println("Element Not Found");
+            System.out.println("Element Not Found Position " + result);
         }
     }
 }
