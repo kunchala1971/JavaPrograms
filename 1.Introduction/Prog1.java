8b3b83a309385957cc4971f295f9c27ddc6af0f2
kunchala1971 <kunchala1971@gmail.com>
2026-03-02 18:24:04 +0530

    updated

--- 1.Introduction/Prog1.java
+++ 1.Introduction/Prog1.java
@@ -3,7 +3,7 @@ class Prog1
 public static void main(String args[])
 {
     System.out.println("Hello Java World!");
-    System.out.println("KKCC INFO SYSTEMS");
+    System.out.println("Telutu IT Tutorials");
     System.out.println("Ongole");    
 }
 }
